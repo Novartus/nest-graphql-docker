0b60ca9b302e4fbea1233792ee0a59d9fd66f919
Abhee Hudani <hudaniabhee@gmail.com>
2021-08-16 22:20:58 +0530

    Feature : GraphQL formatError added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { GraphQLError, GraphQLFormattedError } from 'graphql';
 import { TypeOrmConfig } from './config/typeorm.config';
 import { AuthModule } from './web/auth/auth.module';
 import { AppController } from './app.controller';
@@ -12,6 +13,15 @@ import { CommonModule } from './common/common.module';
     GraphQLModule.forRoot({
       autoSchemaFile: true,
       sortSchema: true,
+      formatError: (error: GraphQLError) => {
+        const graphQLFormattedError: GraphQLFormattedError = {
+          message: error.message || error.extensions.exception.response.message,
+          locations: error.locations,
+          extensions: error.extensions.exception.response,
+          path: error.path,
+        };
+        return graphQLFormattedError;
+      },
       buildSchemaOptions: {
         dateScalarMode: 'timestamp',
       },
